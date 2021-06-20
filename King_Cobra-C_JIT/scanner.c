ef9b71cd7b9b5178e8d7e76a6bfb86aa15efd146
mali5820k <mali5820k@gmail.com>
2021-06-20 01:18:27 -0400

    Before Compiling Expressions

--- King_Cobra-C_JIT/scanner.c
+++ King_Cobra-C_JIT/scanner.c
@@ -19,6 +19,14 @@ void initScanner(const char* source) {
     scanner.line = 1;
 }
 
+static bool isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+}
+
+static bool isDigit(char c) {
+    return c >= '0' && c <= '9';
+}
+
 static bool isAtEnd() {
     return *scanner.current == '\0';
 }
@@ -30,6 +38,11 @@ static char advance() {
 
 static char peek() {
     return *scanner.current;
+}
+
+static char peekNext() {
+    if(isAtEnd()) return '\0';
+    return scanner.current[1];
 }
 
 static bool match(char expected) {
@@ -70,10 +83,93 @@ static void skipWhitespace() {
                 scanner.line++;
                 advance();
                 break;
+            case '/': // Comments are going to be preceded by //
+                if (peekNext() == '/') {
+                    while (peak() != '\n' && !isAtEnd()) advance();
+                } 
+                else {
+                    return;
+                }
+                break;
             default:
                 return;
         }
     }
+}
+
+static TokenType checkKeyword(int start, int length, const char* rest, TokenType type) {
+    if(scanner.current - scanner.start == start + length && memcmp(scanner.start + start, rest, length) == 0) {
+        return type;
+    }
+
+    return TOKEN_IDENTIFIER;
+}
+
+static TokenType identifierType() {
+    switch(scanner.start[0]) {
+        case 'a': return checkKeyword(1, 2, "nd", TOKEN_AND);
+        case 'c': return checkKeyword(1, 4, "lass", TOKEN_CLASS);
+        case 'd': return checkKeyword(1, 2, "ef", TOKEN_FUNCTION); // for def use as a function declaration keyword
+        case 'e': return checkKeyword(1, 3, "lse", TOKEN_ELSE);
+        case 'f': 
+            if(scanner.current - scanner.start > 1) {
+                switch(scanner.start[1]) {
+                    case 'a': return checkKeyword(2, 3, "lse", TOKEN_FALSE);
+                    case 'o': return checkKeyword(2, 1, "r", TOKEN_FOR);
+                    //case 'u': return checkKeyword(2, 6, "nction", TOKEN_FUNCTION); // function
+                }
+            }
+            break;
+        case 'i': return checkKeyword(1, 1, "f", TOKEN_IF);
+        case 'n': return checkKeyword(1, 2, "ot", TOKEN_NOT);
+        case 'o': return checkKeyword(1, 1, "r", TOKEN_OR);
+        case 'p': return checkKeyword(1, 4, "rint", TOKEN_PRINT);
+        case 'r': return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
+        case 's': return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+        case 't': 
+            if(scanner.current - scanner.start > 1) {
+                switch(scanner.start[1]) {
+                    case'h': return checkKeyword(2, 2, "is", TOKEN_THIS);
+                    case 'r': return checkKeyword(2, 2, "ue", TOKEN_TRUE);
+                }
+            }
+            break;
+        case 'v': return checkKeyword(1, 2, "ar", TOKEN_VAR);
+        case 'w': return checkKeyword(1, 4, "hile", TOKEN_WHILE);
+    }
+
+    return TOKEN_IDENTIFIER;
+}
+
+static Token identifier() {
+    while(isAlpha(peek()) || isDigit(peek())) advance();
+    return makeToken(identifierType());
+}
+
+static Token number() {
+    while (isDigit(peek())) advance();
+
+    // Look for the decimal components:
+    if(peek() == '.' && isDigit(peekNext())) {
+        // Consume the "."
+        advance();
+
+        while(isDigit(peek())) advance();
+    }
+
+    return makeToken(TOKEN_NUMBER);
+}
+
+static Token string() {
+    while(peek() != '"' && !isAtEnd()) {
+        if(peek() == '\n') scanner.line++;
+        advance();
+    }
+
+    if(isAtEnd()) return errorToken("Unterminated string."); // Message to print when error occurs
+
+    advance();
+    return makeToken(TOKEN_STRING);
 }
 
 Token scanToken() {
@@ -87,6 +183,8 @@ Token scanToken() {
 
     // For single character tokens:
     char c = advance();
+    if(isAlpha(c)) return identifier();
+    if(isDigit(c)) return number();
 
     switch(c) {
         case '(': return makeToken(TOKEN_LEFT_PAREN);
@@ -110,7 +208,8 @@ Token scanToken() {
             return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
         case '>':
             return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
-        
+        case '"': // For string recognition
+            return string();
         
     }
 
