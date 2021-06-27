08c34c8ec27c2235a71e0175e7f12e38e410fb6b
mali5820k <mali5820k@gmail.com>
2021-06-26 21:16:29 -0400

    Prefix and Infix Parsing

--- King_Cobra-C_JIT/compiler.c
+++ King_Cobra-C_JIT/compiler.c
@@ -25,6 +25,14 @@ typedef enum {
     PREC_PRIMARY
 } Precedence;
 
+typedef void (*ParseFn)(); // Function Pointer Type
+
+typedef struct {
+    ParseFn prefix;
+    ParseFn infix;
+    Precedence precedence;
+} ParseRule;
+
 Parser parser;
 Chunk* compilingChunk;
 
@@ -32,6 +40,8 @@ static Chunk* currentChunk() {
     return compilingChunk;
 }
 
+// Handler of an error. Prints out the message and prepares to break out of compiling
+// by setting up error flags in the parser.
 static void errorAt(Token* token, const char* message) {
     if(parser.panicMode) return; // To prevent any cascading errors from source error from being reported
     parser.panicMode = true;
@@ -51,14 +61,18 @@ static void errorAt(Token* token, const char* message) {
     parser.hadError = true;
 }
 
+// Output error with message
 static void error(const char* message) {
     errorAt(&parser.previous, message);
 }
 
+// Output message for error at current token
 static void errorAtCurrent(const char* message) {
     errorAt(&parser.current, message);
 }
 
+// Parses through the current token and stops.
+// Will handle error if one is encountered.
 static void advance() {
     parser.previous = parser.current;
 
@@ -70,6 +84,7 @@ static void advance() {
     }
 }
 
+// Consumes current Token or outputs error message
 static void consume(TokenType type, const char* message) {
     if(parser.current.type == type) {
         advance();
@@ -83,11 +98,13 @@ static void emitByte(uint8_t byte) {
     writeChunk(currentChunk(), byte, parser.previous.line);
 }
 
+// Emits Bytes for input bytes 1 and 2
 static void emitBytes(uint8_t byte1, uint8_t byte2) {
     emitByte(byte1);
     emitByte(byte2);
 }
 
+// Return instruction
 static void emitReturn() {
     emitByte(OP_RETURN);
 }
@@ -104,12 +121,43 @@ static uint8_t makeConstant(Value value) {
     return (uint8_t)constant;
 }
 
+// Handles emitting a constant number
 static void emitConstant(Value value) {
     emitBytes(OP_CONSTANT, makeConstant(value));
 }
 
+// Ends the compiling process
 static void endCompiler() {
     emitReturn();
+}
+
+// Function prototypes for these select methods
+static void expression();
+static ParseRule* getRule(TokenType type);
+static void parsePrecedence(Precedence precedence);
+
+// For Mathematical operators
+static void binary() {
+    TokenType operatorType = parser.previous.type;
+    ParseRule* rule = getRule(operatorType);
+    parsePrecedence((Precedence)(rule -> precedence + 1));
+
+    switch(operatorType) {
+        case TOKEN_PLUS: 
+            emitByte(OP_ADD);
+            break;
+        case TOKEN_MINUS:
+            emitByte(OP_SUBTRACT);
+            break;
+        case TOKEN_STAR:
+            emitByte(OP_MULTIPLY);
+            break;
+        case TOKEN_SLASH:
+            emitByte(OP_DIVIDE);
+            break;
+        default:
+            return; // Shouldn't reach this part ever!
+    }
 }
 
 // For parenthesis grouping. May need to edit the error messages later
@@ -118,6 +166,7 @@ static void grouping() {
     consume(TOKEN_RIGHT_PAREN, "expect ')' after expression.");
 }
 
+// Emmitting constant numerical values
 static void number() {
     double value = strtod(parser.previous.start, NULL);
     emitConstant(value);
@@ -138,8 +187,73 @@ static void unary() {
     }
 }
 
-static void parsePrecedence(Precedence precedence) {
+// Lookup Designated initializer syntax in C
+// This is a rules look-up table
+ParseRule rules[] = {
+  [TOKEN_LEFT_PAREN]    = {grouping, NULL,   PREC_NONE},
+  [TOKEN_RIGHT_PAREN]   = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_LEFT_BRACE]    = {NULL,     NULL,   PREC_NONE}, 
+  [TOKEN_RIGHT_BRACE]   = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_COMMA]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_DOT]           = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_MINUS]         = {unary,    binary, PREC_TERM},
+  [TOKEN_PLUS]          = {NULL,     binary, PREC_TERM},
+  [TOKEN_SEMICOLON]     = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_SLASH]         = {NULL,     binary, PREC_FACTOR},
+  [TOKEN_STAR]          = {NULL,     binary, PREC_FACTOR},
+  [TOKEN_BANG]          = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_BANG_EQUAL]    = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_EQUAL]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_EQUAL_EQUAL]   = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_GREATER]       = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_GREATER_EQUAL] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_LESS]          = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_LESS_EQUAL]    = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_IDENTIFIER]    = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_STRING]        = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_NUMBER]        = {number,   NULL,   PREC_NONE},
+  [TOKEN_AND]           = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_CLASS]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_ELSE]          = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_FALSE]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_FOR]           = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_FUNCTION]      = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_IF]            = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_NOT]           = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_OR]            = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_PRINT]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_RETURN]        = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_SUPER]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_THIS]          = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_TRUE]          = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_VAR]           = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_WHILE]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_ERROR]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_EOF]           = {NULL,     NULL,   PREC_NONE},
+};
 
+// Read the next token and then look up the rule for that token.
+// If a rule isn't found for a token, then that's a syntax error.
+// Both the prefix and infix parts of an expression get parsed.
+static void parsePrecedence(Precedence precedence) {
+    advance();
+    ParseFn prefixRule = getRule(parser.previous.type) -> prefix;
+    if(prefixRule == NULL) {
+        error("Expect expression.");
+        return;
+    }
+
+    prefixRule();
+    while(precedence <= getRule(parser.current.type) -> precedence) {
+        advance();
+        ParseFn infixRule = getRule(parser.previous.type) -> infix;
+        infixRule();
+    }
+}
+
+// Looks up the rule for a particular token type
+static ParseRule* getRule(TokenType type) {
+    return &rules[type];
 }
 
 static void expression() {
