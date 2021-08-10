41a66f69c617e2ba3b13b967a1b89e6a17cdc83e
mali5820k <mali5820k@gmail.com>
2021-08-10 06:40:30 -0400

    Finished adding loops
    Also includes logical operators or and and.

--- King_Cobra-C_JIT/vm.c
+++ King_Cobra-C_JIT/vm.c
@@ -75,6 +75,8 @@ void freeVM() {
 static InterpretResult run() {
     #define READ_BYTE() (*vm.ip++)
     #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+    #define READ_SHORT() \
+        (vm.ip += 2, (uint16_t)((vm.ip[-2] << 8) | vm.ip[-1]))
     #define READ_STRING() AS_STRING(READ_CONSTANT())
 
     // This checks that both operands are numbers, otherwise, we throw a runtime error
@@ -192,6 +194,21 @@ static InterpretResult run() {
                 printf("\n");
                 break;
             }
+            case OP_JUMP: {
+                uint16_t offset = READ_SHORT();
+                vm.ip += offset;
+                break;
+            }
+            case OP_JUMP_IF_FALSE: {
+                uint16_t offset = READ_SHORT();
+                if(isFalsey(peek(0))) vm.ip += offset;
+                break;
+            }
+            case OP_LOOP: {
+                uint16_t offset  = READ_SHORT();
+                vm.ip -= offset;
+                break;
+            }
             case OP_RETURN: {
                 return INTERPRET_OK;
             }
@@ -199,6 +216,7 @@ static InterpretResult run() {
     }
 
     #undef READ_BYTE
+    #undef READ_SHORT
     #undef READ_CONSTANT
     #undef READ_STRING
     #undef BINARY_OP
