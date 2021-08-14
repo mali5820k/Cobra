441a3cb8d9bd2c20a866df56a5f41cb8ee622193
mali5820k <mali5820k@gmail.com>
2021-08-14 11:41:08 -0400

    WIP Adding function support

--- King_Cobra-C_JIT/object.h
+++ King_Cobra-C_JIT/object.h
@@ -2,6 +2,7 @@
 #define kc_object_h
 
 #include "common.h"
+#include "chunk.h"
 #include "value.h"
 
 // A macro that extracts the type from a specific value.
@@ -10,16 +11,19 @@
 /**
  * Macros to check if the provided value is of a specific type of object.
 */
+#define IS_FUNCTION(value)  isObjType(value, OBJ_FUNCTION)
 #define IS_STRING(value)    isObjType(value, OBJ_STRING)
 
 /**
  * The AS_STRING macro returns the pointer to that string object,
  * and the AS_CSTRING macro returns the character array of that string object.
 */
+#define AS_FUNCTION(value)  ((ObjFunction*)AS_OBJ(value))
 #define AS_STRING(value)    ((ObjString*)AS_OBJ(value))
 #define AS_CSTRING(value)   (((ObjString*)AS_OBJ(value)) -> chars)
 
 typedef enum {
+    OBJ_FUNCTION,
     OBJ_STRING,
 } ObjType;
 
@@ -28,6 +32,13 @@ struct Obj {
     struct Obj* next;
 };
 
+typedef struct {
+    Obj obj;
+    int arity;
+    Chunk chunk;
+    ObjString* name;
+} ObjFunction;
+
 struct ObjString {
     Obj obj;
     int length;
@@ -35,6 +46,7 @@ struct ObjString {
     uint32_t hash;
 };
 
+ObjFunction* newFunction();
 ObjString* takeString(char* chars, int length);
 ObjString* copyString(const char* chars, int length);
 void printObject(Value value);
