06327bc0f63d715a5f8c53e18ad4a56a48f8baff
mali5820k <mali5820k@gmail.com>
2021-10-06 01:40:37 -0400

    Created C# files for starting point

---
+++ King_Cobra_C#_NET_Compiler/Lexer.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KC_Language {
+
+	public class Lexer {
+		public Lexer() {
+
+		}
+
+		public static Token Tokenize() {
+			Token newToken = new Token();
+			return newToken;
+		}
+	}
+	
+}
---
+++ King_Cobra_C#_NET_Compiler/MainProgram.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KC_Language {
+
+	public class MainProgram {
+		public static void Main(string[] args) {
+			Console.Writeline("C#");
+		}
+	}
+	
+}
